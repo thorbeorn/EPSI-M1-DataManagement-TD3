f4b39ae6b20c49355a2b61871efeb2cb4f2f4ffe
Thorbeorn <dylanllodrapro@gmail.com>
2025-11-16 23:27:19 +0100

    all code without unit test

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import pandera.pandas as pa
 from pandera import Column, Check
+from pandera.errors import SchemaErrors
 from datetime import datetime
 import os
 import json
 
 PATHS = {
-    "csv": "data/input/users_raw.csv"
+    "csv": "data/input/users_raw.csv",
+    "trusted_dir" : "data/trusted",
+    "quarantine_dir": "data/quarantine",
+    "alert_dir": "data/alerts"
 }
 
 def extract_Dataframe_From_CSV(path):
@@ -23,115 +27,105 @@ def lowercase_Dataframe_Column(dataframe, column):
 def replace_Dataframe_Column_Not_Numeric_To_NULL(dataframe, column):
     if column not in dataframe.columns:
         raise ValueError(f"lowercase_Dataframe_Column: {column} is not in dataframe columns")
-    dataframe[column] = pd.to_numeric(dataframe[column], errors='coerce')
+    dataframe[column] = pd.to_numeric(dataframe[column], errors='coerce', downcast='integer').astype("int64")
     return dataframe
 def replace_Dataframe_Column_Not_Datetime_To_NULL(dataframe, column):
     if column not in dataframe.columns:
         raise ValueError(f"lowercase_Dataframe_Column: {column} is not in dataframe columns")
     dataframe[column] = pd.to_datetime(dataframe[column], errors='coerce')
     return dataframe
-def replace_NaN_By_NULL(dataframe):
-    dataframe = dataframe.replace(r'^\s*$', pd.NA, regex=True)
-    dataframe = dataframe.fillna("NULL")
-    return dataframe
 
 def transform_data(dataframe) :
     if not isinstance(dataframe, pd.DataFrame):
         raise ValueError("transform_data: input is not a pandas DataFrame")
 
-    #No transformation for user_id
-
+    dataframe = replace_Dataframe_Column_Not_Numeric_To_NULL(dataframe, "user_id")
     dataframe = lowercase_Dataframe_Column(dataframe, "username")
     dataframe = lowercase_Dataframe_Column(dataframe, "email")
     dataframe = replace_Dataframe_Column_Not_Numeric_To_NULL(dataframe, "age")
     dataframe = replace_Dataframe_Column_Not_Datetime_To_NULL(dataframe, "signup_date")
 
-    dataframe = replace_NaN_By_NULL(dataframe)
-
     return dataframe
-
 def validate_data(dataframe):
-    state = False
     schema = pa.DataFrameSchema(
         {
             "user_id": Column(
-                checks=Check.not_equal_to("NULL"),
-                nullable=False
+                int,
+                unique=True,
+                checks=Check.not_equal_to("nan"),
+                nullable=True
             ),
             "username": Column(
-                checks=Check.not_equal_to("NULL"),
+                str,
+                checks=Check.not_equal_to("nan"),
                 nullable=True
             ),
             "age": Column(
-                checks=Check.between(18, 100),
+                int,
+                checks=Check.between(-20, 100),
                 nullable=True
             ),
             "email": Column(
+                str,
                 checks=Check.str_matches(
                     r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
                 ),
-                nullable=False
+                nullable=True
             ),
             "signup_date": Column(
-                checks=Check.not_equal_to("NULL"),
+                datetime,
+                checks=Check.not_equal_to("nan"),
                 nullable=True
             )
         },
-        strict=False
+        strict=True
     )
-    
-    dataframetemp = dataframe.copy()
-    dataframetemp['validate'] = True
+    try:
+        schema.validate(dataframe, lazy=True)
+        return dataframe, pd.DataFrame(), None
+    except SchemaErrors as e:
+        failure_cases = e.failure_cases
+        failed_indices = failure_cases['index'].unique()
+        failed_dataframe = dataframe.loc[failed_indices].copy()
+        valid_dataframe = dataframe.drop(failed_indices).copy()
+        return valid_dataframe, failed_dataframe, e
 
-    errors_json_list = []
-    for idx, row in dataframetemp.iterrows():
-        try:
-            schema.validate(pd.DataFrame([row]), lazy=False)
-        except pa.errors.SchemaError as e:
-            errors_json_list.append({idx: e})
-            dataframetemp.at[idx, 'validate'] = False
-            state = True
-
-
-    duplicated_ids = dataframetemp['user_id'][dataframetemp['user_id'].duplicated(keep=False)]
-    duplicated_indices = dataframetemp['user_id'][dataframetemp['user_id'].duplicated(keep=False)].index
-    dataframetemp.loc[dataframetemp['user_id'].isin(duplicated_ids), 'validate'] = False
-    for index in duplicated_indices.to_list():
-        errors_json_list.append({index: "is duplicated"})
-
-    df_valid = dataframetemp[dataframetemp['validate'] == True].reset_index(drop=True)
-    df_invalid = dataframetemp[dataframetemp['validate'] == False].reset_index(drop=True)
-
-    return df_valid, df_invalid, state, errors_json_list
-
-def send_alert(message):
+def send_Alert_JSON(message, path):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_dir = "data/alerts"
-    os.makedirs(output_dir, exist_ok=True)
-    filename = os.path.join(output_dir, f"alert_{timestamp}.txt")
+    os.makedirs(path, exist_ok=True)
+    filename = os.path.join(path, f"alert_{timestamp}.json")
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(message, f, indent=2) 
+def send_Alert_TXT(message, path):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    os.makedirs(path, exist_ok=True)
+    filename = os.path.join(path, f"alert_{timestamp}.txt")
     with open(filename, "a", encoding="utf-8") as f:
         f.write(message + "\n")
+def Dataframe_To_Parquet_Write(dataframe, path):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    os.makedirs(path, exist_ok=True)
+    filename = os.path.join(path, f"dylan_{timestamp}.parquet")
+    dataframe.to_parquet(filename, engine='pyarrow', index=False)
 
 def run_pipeline():
-    dataframeRaw = extract_Dataframe_From_CSV(PATHS["csv"])
+    dataframe = extract_Dataframe_From_CSV(PATHS["csv"])
     print("Data extraction successful.")
 
-    Tdataframe = transform_data(dataframeRaw)
+    dataframe = transform_data(dataframe)
+    print("Data transformation successful.")
 
-    VVdataframe, VIdataframe, state, errors_json_list = validate_data(Tdataframe)
-    if state is False:
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        output_dir = "data/trusted"
-        os.makedirs(output_dir, exist_ok=True)
-        filename = os.path.join(output_dir, "dylan_{timestamp}.parquet")
-        VVdataframe.to_parquet(filename, engine='pyarrow', index=False)
-        print("Data Quality Check Passed. Pipeline finished successfully.")
-    else :
-        output_dir = "data/quarantine"
-        os.makedirs(output_dir, exist_ok=True)
-        filename = os.path.join(output_dir, "sales.parquet")
-        VIdataframe.to_parquet(filename, engine='pyarrow', index=False)
-        send_alert("CRITICAL: ETL Pipeline failed for users_raw.csv. See quarantine for details.")
+    valid_df, failed_df, exception_error = validate_data(dataframe)
+    if exception_error is not None:
+        if not valid_df.empty:
+            Dataframe_To_Parquet_Write(valid_df, PATHS["trusted_dir"])
+        if not failed_df.empty:
+            Dataframe_To_Parquet_Write(failed_df, PATHS["quarantine_dir"])
+        send_Alert_TXT("CRITICAL: ETL Pipeline failed for users_raw.csv. See json for details.", PATHS["alert_dir"])
+        send_Alert_JSON(exception_error.message, PATHS["alert_dir"])
         print("ON VIOLATION")
+    else:
+        Dataframe_To_Parquet_Write(valid_df, PATHS["trusted_dir"])
+        print("Data Quality Check Passed. Pipeline finished successfully.")
 
 run_pipeline()
